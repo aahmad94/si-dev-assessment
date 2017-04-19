ed352ec150b9da6f04ce8a1e37b02e9e0c4f4baa
Adeel <aahmad5394@gmail.com>
2017-04-19 05:27:32 +0000

    Finishing Task 6 and adding to Task 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
 
     account_sid = ENV["ACCOUNT_SID"] # Your Account SID from www.twilio.com/user/account
     auth_token = ENV["AUTH_TOKEN"]   # Your Auth Token from www.twilio.com/user/account
-
+    
     @client = Twilio::REST::Client.new(account_sid, auth_token)
     message = @client.account.messages.create(body: params[:message],
       from: ENV["TWILIO_NUMBER"],
