1080bdab87bb59f43e03874cf403860c41405cd5
aahmad94 <aka5vm@virginia.edu>
2018-08-13 17:13:59 -0700

    change app name, fix pages#sms route handler

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,17 @@ class PagesController < ApplicationController
   require 'twilio-ruby'
 
   def sms
-    #byebug
-    #raise "Action sms in the UsersController is not implemented! received params #{params.to_json}"
-
-    account_sid = ENV["ACCOUNT_SID"] # Your Account SID from www.twilio.com/user/account
-    auth_token = ENV["AUTH_TOKEN"]   # Your Auth Token from www.twilio.com/user/account
+    account_sid = "ACe4a5ff15f25259d20b84d04077fb655b"
+    # ENV["ACCOUNT_SID"] # Account SID from www.twilio.com/user/account
+    auth_token = "eb3418b3dd5bd0040144f1e7cad76933"
+    # ENV["AUTH_TOKEN"]   # Auth Token from www.twilio.com/user/account
     
     @client = Twilio::REST::Client.new(account_sid, auth_token)
-    message = @client.account.messages.create(body: params[:message],
-      from: ENV["TWILIO_NUMBER"],
+    @client.account.messages.create(
+      body: params[:message],
+      from: "+1 415 549 9844", 
       to: params[:number]
-    )
+      )
+    end
   end
-end
+  # ENV["TWILIO_NUMBER"],
