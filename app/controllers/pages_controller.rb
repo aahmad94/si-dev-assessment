1acd1b5501c53ed3d90b21bc4636bb1a7702ff21
aahmad94 <aka5vm@virginia.edu>
2018-08-13 22:30:03 -0700

    fix api connection

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,17 +5,14 @@ class PagesController < ApplicationController
   require 'twilio-ruby'
 
   def sms
-    account_sid = "ACe4a5ff15f25259d20b84d04077fb655b"
-    # ENV["ACCOUNT_SID"] # Account SID from www.twilio.com/user/account
-    auth_token = "eb3418b3dd5bd0040144f1e7cad76933"
-    # ENV["AUTH_TOKEN"]   # Auth Token from www.twilio.com/user/account
+    account_sid = ENV["ACCOUNT_SID"] # Account SID from www.twilio.com/user/account
+    auth_token = ENV["AUTH_TOKEN"]   # Auth Token from www.twilio.com/user/account
     
     @client = Twilio::REST::Client.new(account_sid, auth_token)
     @client.account.messages.create(
       body: params[:message],
-      from: "+1 415 549 9844", 
+      from: ENV["TWILIO_NUMBER"],
       to: params[:number]
       )
-    end
   end
-  # ENV["TWILIO_NUMBER"],
+end
