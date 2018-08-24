7bbc53e1ede6fddb924d4c3916055b41ba001181
aahmad94 <aka5vm@virginia.edu>
2018-08-23 22:48:47 -0700

    remove rack cors middleware

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'rack/cors'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,12 +11,5 @@ module SmsApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :delete]
-      end
-    end
-    config.autoload_paths << Rails.root.join('lib')
   end
 end
